f2c41e4d2e1aed05fa7e5ece86caffce1b0473d2
ferruzxca <ferruzxca@gmail.com>
2026-02-06 07:58:53 -0600

    Fix Laravel view cache directory on boot

--- laravel_front/start.sh
+++ laravel_front/start.sh
@@ -8,6 +8,10 @@ fi
 php /app/artisan key:generate --force
 php /app/artisan config:clear
 php /app/artisan route:clear
+
+if [ ! -d /app/storage/framework/views ]; then
+  mkdir -p /app/storage/framework/views
+fi
 php /app/artisan view:clear
 
 exec php -S 0.0.0.0:${PORT} -t /app/public
