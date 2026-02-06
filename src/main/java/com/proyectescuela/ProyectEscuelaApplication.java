cbd22af42cf8f94479fa15f7f0c5a29b1edb8442
ferruzxca <ferruzxca@gmail.com>
2026-02-06 00:34:05 -0600

    Fix component scan root package

--- src/main/java/com/proyectescuela/ProyectEscuelaApplication.java
+++ src/main/java/com/proyectescuela/ProyectEscuelaApplication.java
@@ -1,4 +1,4 @@
-package com.proyectescuela.api;
+package com.proyectescuela;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
